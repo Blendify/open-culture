599be65f533193d1ba76af595ecfe6f5c0900c73
Aaron Carlisle <carlisle.b3d@gmail.com>
2019-03-30 14:25:59 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -11,10 +11,10 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'exts')))
 
 
 # -- Project information -----------------------------------------------------
@@ -33,12 +33,13 @@ release = '0.0.1'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'youtube',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
